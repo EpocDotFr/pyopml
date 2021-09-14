2d041d92e462ebfd3e47ad1e056355332e362be0
EpocDotFr <contact@epoc.fr>
2021-09-14 13:52:46 +0200

    Begin implementing OPML file/string reading

--- opml/document.py
+++ opml/document.py
@@ -1,4 +1,4 @@
-from email.utils import format_datetime as datetime_to_rfc2822
+from email.utils import format_datetime as datetime_to_rfc2822, parsedate_to_datetime as rfc2822_to_datetime
 from .outlinable import Outlinable
 from lxml import etree
 
@@ -35,6 +35,89 @@ class OpmlDocument(Outlinable):
             encoding=encoding,
             xml_declaration=xml_declaration
         )
+
+    def loads(self, s):
+        self.unbuild_tree(
+            etree.fromstring(s)
+        )
+
+    def load(self, fp):
+        self.unbuild_tree(
+            etree.parse(fp).getroot()
+        )
+
+    def unbuild_tree(self, root):
+        version = root.get('version')
+
+        if not version:
+            raise ValueError('"version" attribute not found in root node')
+        elif version != '2.0':
+            raise ValueError('This package only supports OPML 2.0 specification')
+
+        head = root.find('head')
+
+        if not head:
+            raise ValueError('"head" node not found')
+
+        title = head.findtext('title')
+
+        if title:
+            self.title = title
+
+        date_created = head.findtext('dateCreated')
+
+        if date_created:
+            self.date_created = rfc2822_to_datetime(date_created)
+
+        date_modified = head.findtext('dateModified')
+
+        if date_modified:
+            self.date_modified = rfc2822_to_datetime(date_modified)
+
+        owner_name = head.findtext('ownerName')
+
+        if owner_name:
+            self.owner_name = owner_name
+
+        owner_email = head.findtext('ownerEmail')
+
+        if owner_email:
+            self.owner_email = owner_email
+
+        owner_id = head.findtext('ownerId')
+
+        if owner_id:
+            self.owner_id = owner_id
+
+        expansion_state = head.findtext('expansionState')
+
+        if expansion_state:
+            self.expansion_state = expansion_state.split(',')
+
+        vert_scroll_state = head.findtext('vertScrollState')
+
+        if vert_scroll_state:
+            self.vert_scroll_state = vert_scroll_state
+
+        window_top = head.findtext('windowTop')
+
+        if window_top:
+            self.window_top = window_top
+
+        window_left = head.findtext('windowLeft')
+
+        if window_left:
+            self.window_left = window_left
+
+        window_bottom = head.findtext('windowBottom')
+
+        if window_bottom:
+            self.window_bottom = window_bottom
+
+        window_right = head.findtext('windowRight')
+
+        if window_right:
+            self.window_right = window_right
 
     def build_tree(self):
         root = etree.Element('opml', version='2.0')
