86841a840acb988210452b1492e7ca184d72a924
EpocDotFr <contact@epoc.fr>
2021-09-16 13:57:43 +0200

    Init gh-pages branch

--- opml/__init__.py
+++
@@ -1,4 +0,0 @@
-from .document import OpmlDocument
-from .outline import OpmlOutline
-
-__version__ = '1.0.0'
--- opml/exceptions.py
+++
@@ -1,8 +0,0 @@
-class OpmlReadError(Exception):
-    """This exception is thrown when a blocking OPML-related read error is encountered."""
-    pass
-
-
-class OpmlWriteError(Exception):
-    """This exception is thrown when a blocking OPML-related write error is encountered."""
-    pass
