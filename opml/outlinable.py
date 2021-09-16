05f45adc83a1bde37f14387c04828d7e891ad854
EpocDotFr <contact@epoc.fr>
2021-09-16 13:33:49 +0200

    Use proper methods signatures with allowed attributes

--- opml/outlinable.py
+++ opml/outlinable.py
@@ -28,20 +28,19 @@ class Outlinable:
 
         return outline
 
-    def add_rss(self, text, xml_url, **kvargs):
+    def add_rss(self, text, xml_url, description=None, html_url=None, language=None, title=None, version=None, is_comment=False, is_breakpoint=False, created=None, categories=[]):
         """Create a new outline of type "rss", append it to this object's outlines and return it.
 
         :param str text: Text of the outline
         :param str xml_url: URL to the feed when ``type`` is ``rss``
-        :param bool is_comment: Whether the outline is commented or not
-        :param bool is_breakpoint: Whether a breakpoint is set on this outline
-        :param datetime.datetime created: Date-time that the outline node was created
         :param str description: Top-level description element from the feed (if this outline is part of a subscription list)
         :param str html_url: Top-level link element from the feed (if this outline is part of a subscription list)
         :param str language: Top-level language element from the feed (if this outline is part of a subscription list)
         :param str title: Top-level title element from the feed (if this outline is part of a subscription list)
         :param str version: RSS version when ``type`` is ``rss``. One of ``RSS``, ``RSS1``, ``RSS2`` or ``scriptingNews``
-        :param str url: An URL to a web page when ``type`` is ``link``
+        :param bool is_comment: Whether the outline is commented or not
+        :param bool is_breakpoint: Whether a breakpoint is set on this outline
+        :param datetime.datetime created: Date-time that the outline node was created
         :param list categories: A list of `RSS 2.0 <https://validator.w3.org/feed/docs/rss2.html#ltcategorygtSubelementOfLtitemgt>`__ categories. To represent a "tag", the category string should contain no slashes
         :rtype: opml.OpmlOutline
         """
@@ -49,24 +48,25 @@ class Outlinable:
             text,
             type='rss',
             xml_url=xml_url,
-            **kvargs
+            description=description,
+            html_url=html_url,
+            language=language,
+            title=title,
+            version=version,
+            is_comment=is_comment,
+            is_breakpoint=is_breakpoint,
+            created=created,
+            categories=categories
         )
 
-    def add_link(self, text, url, **kvargs):
+    def add_link(self, text, url, is_comment=False, is_breakpoint=False, created=None, categories=[]):
         """Create a new outline of type "link", append it to this object's outlines and return it.
 
         :param str text: Text of the outline
         :param str url: An URL to a web page when ``type`` is ``link``
-        :param str type: How the other attributes of the outline are interpreted. One of ``rss``, ``link`` or ``include``
         :param bool is_comment: Whether the outline is commented or not
         :param bool is_breakpoint: Whether a breakpoint is set on this outline
         :param datetime.datetime created: Date-time that the outline node was created
-        :param str xml_url: URL to the feed when ``type`` is ``rss``
-        :param str description: Top-level description element from the feed (if this outline is part of a subscription list)
-        :param str html_url: Top-level link element from the feed (if this outline is part of a subscription list)
-        :param str language: Top-level language element from the feed (if this outline is part of a subscription list)
-        :param str title: Top-level title element from the feed (if this outline is part of a subscription list)
-        :param str version: RSS version when ``type`` is ``rss``. One of ``RSS``, ``RSS1``, ``RSS2`` or ``scriptingNews``
         :param list categories: A list of `RSS 2.0 <https://validator.w3.org/feed/docs/rss2.html#ltcategorygtSubelementOfLtitemgt>`__ categories. To represent a "tag", the category string should contain no slashes
         :rtype: opml.OpmlOutline
         """
@@ -74,24 +74,20 @@ class Outlinable:
             text,
             type='link',
             url=url,
-            **kvargs
+            is_comment=is_comment,
+            is_breakpoint=is_breakpoint,
+            created=created,
+            categories=categories
         )
 
-    def add_include(self, text, url, **kvargs):
+    def add_include(self, text, url, is_comment=False, is_breakpoint=False, created=None, categories=[]):
         """Create a new outline of type "include", append it to this object's outlines and return it.
 
         :param str text: Text of the outline
         :param str url: An URL to a web page when ``type`` is ``link``
-        :param str type: How the other attributes of the outline are interpreted. One of ``rss``, ``link`` or ``include``
         :param bool is_comment: Whether the outline is commented or not
         :param bool is_breakpoint: Whether a breakpoint is set on this outline
         :param datetime.datetime created: Date-time that the outline node was created
-        :param str xml_url: URL to the feed when ``type`` is ``rss``
-        :param str description: Top-level description element from the feed (if this outline is part of a subscription list)
-        :param str html_url: Top-level link element from the feed (if this outline is part of a subscription list)
-        :param str language: Top-level language element from the feed (if this outline is part of a subscription list)
-        :param str title: Top-level title element from the feed (if this outline is part of a subscription list)
-        :param str version: RSS version when ``type`` is ``rss``. One of ``RSS``, ``RSS1``, ``RSS2`` or ``scriptingNews``
         :param list categories: A list of `RSS 2.0 <https://validator.w3.org/feed/docs/rss2.html#ltcategorygtSubelementOfLtitemgt>`__ categories. To represent a "tag", the category string should contain no slashes
         :rtype: opml.OpmlOutline
         """
@@ -99,7 +95,10 @@ class Outlinable:
             text,
             type='include',
             url=url,
-            **kvargs
+            is_comment=is_comment,
+            is_breakpoint=is_breakpoint,
+            created=created,
+            categories=categories
         )
 
     def build_outlines_tree(self, parent):
