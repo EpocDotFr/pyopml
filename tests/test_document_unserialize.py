db1e0d1f045425f26998afbd62d782a76467d671
EpocDotFr <contact@epoc.fr>
2021-09-17 16:50:17 +0200

    Moar tests

--- tests/test_document_unserialize.py
+++ tests/test_document_unserialize.py
@@ -1,5 +1,6 @@
+from opml import OpmlDocument, OpmlOutline
 from opml.exceptions import OpmlReadError
-from opml import OpmlDocument
+from datetime import datetime
 import pytest
 import re
 
@@ -17,6 +18,80 @@ def run_read_error_suite(filename, error_message):
     with pytest.raises(OpmlReadError, match=error_message):
         with open(filename, 'r') as f:
             OpmlDocument.load(f)
+
+
+def validate_valid_opml_document(document):
+    assert isinstance(document, OpmlDocument)
+    assert document.title == 'Hendley Associates Feed'
+    assert isinstance(document.date_created, datetime)
+    assert isinstance(document.date_modified, datetime)
+    assert document.owner_name == 'Gerry Hendley'
+    assert document.owner_email == 'gerry@hendley-associates.com'
+    assert document.owner_id == 'https://hendley-associates.com'
+    assert document.expansion_state == ['2', '5']
+    assert document.vert_scroll_state == '4'
+    assert document.window_top == '50'
+    assert document.window_left == '50'
+    assert document.window_bottom == '50'
+    assert document.window_right == '50'
+
+    assert len(document.outlines) == 3
+
+    # Feeds
+
+    feeds_outline = document.outlines[0]
+
+    assert feeds_outline.text == 'Feeds'
+
+    assert len(feeds_outline.outlines) == 1
+
+    rss_outline = feeds_outline.outlines[0]
+
+    assert isinstance(rss_outline, OpmlOutline)
+    assert rss_outline.text == 'CIA News Feed'
+    assert rss_outline.type == 'rss'
+    assert isinstance(rss_outline.created, datetime)
+    assert rss_outline.xml_url == 'https://hendley-associates.com/feeds/cia.rss'
+    assert rss_outline.description == 'CIA News Feed'
+    assert rss_outline.html_url == 'https://hendley-associates.com/news/cia'
+    assert rss_outline.language == 'en_US'
+    assert rss_outline.title == 'CIA News Feed'
+    assert rss_outline.version == 'RSS2'
+    assert rss_outline.categories == ['/Intelligence/USA', 'intelligence']
+
+    # Links
+
+    links_outline = document.outlines[1]
+
+    assert links_outline.text == 'Links'
+
+    assert len(links_outline.outlines) == 1
+
+    link_outline = links_outline.outlines[0]
+
+    assert isinstance(link_outline, OpmlOutline)
+    assert link_outline.text == 'Jack Ryan re-elected for second mandate'
+    assert link_outline.type == 'link'
+    assert isinstance(link_outline.created, datetime)
+    assert link_outline.url == 'https://hendley-associates.com/articles/usa/2021/08/02/jack-ryan-re-elected-second-mandate.html'
+    assert link_outline.categories == ['/Intelligence/USA', 'intelligence']
+
+    # Includes
+
+    includes_outline = document.outlines[2]
+
+    assert includes_outline.text == 'Includes'
+
+    assert len(includes_outline.outlines) == 1
+
+    include_outline = includes_outline.outlines[0]
+
+    assert isinstance(include_outline, OpmlOutline)
+    assert include_outline.text == 'All Feeds'
+    assert include_outline.type == 'include'
+    assert isinstance(include_outline.created, datetime)
+    assert include_outline.url == 'https://hendley-associates.com/feeds.opml'
+    assert include_outline.categories == ['/Intelligence/USA', 'intelligence']
 
 
 def test_not_an_opml_document():
@@ -95,9 +170,9 @@ def test_valid():
     with open(filename, 'r') as f:
         file_as_string = f.read()
 
-    assert isinstance(OpmlDocument.loads(file_as_string), OpmlDocument)
+    validate_valid_opml_document(OpmlDocument.loads(file_as_string))
 
-    assert isinstance(OpmlDocument.load(filename), OpmlDocument)
+    validate_valid_opml_document(OpmlDocument.load(filename))
 
     with open(filename, 'r') as f:
-        assert isinstance(OpmlDocument.load(f), OpmlDocument)
+        validate_valid_opml_document(OpmlDocument.load(f))
